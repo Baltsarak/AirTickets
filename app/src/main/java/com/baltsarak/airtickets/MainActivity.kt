dd7e8fedb1f234b9f76b3d5897ebb31f81d25ade
Baltsarak <evgeniy.bk.89@gmail.com>
2024-06-27 22:47:23 +0300

    Создание нижней панели навигации

--- app/src/main/java/com/baltsarak/airtickets/MainActivity.kt
+++ app/src/main/java/com/baltsarak/airtickets/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.baltsarak.airtickets
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnItemSelectedListener(onItemSelectedListener)
+    }
+
+    private val onItemSelectedListener = NavigationBarView.OnItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_air_tickets -> {
+
+                true
+            }
+
+            R.id.navigation_hotels -> {
+
+                true
+            }
+
+            R.id.navigation_in_short -> {
+
+                true
+            }
+
+            R.id.navigation_subscriptions -> {
+
+                true
+            }
+
+            R.id.navigation_profile -> {
+
+                true
+            }
+
+            else -> false
+        }
     }
 }
